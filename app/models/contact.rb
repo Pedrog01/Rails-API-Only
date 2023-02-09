d6a76498fb90569720e9766a4787fcf5bff8f11a
Pedrog01 <pedro7109.j@gmail.com>
2023-02-09 10:26:52 -0300

    I18n for date

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,28 +1,39 @@
 class Contact < ApplicationRecord
     belongs_to :kind #, optional: true
 
+    #def brithdate_br
+    # I18n.l(self.birthdate) unless self.birthdate.blank?
+    #end
+
+    def to_br
+        {
+            name: self.name,
+            email: self.email,
+            birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+        }
+    end
 
     #def author
-     #   "Jacksin Pires"
+    #   "Jacksin Pires"
     #end
 
     # def kind_description
-     #   self.kind.description
+    #   self.kind.description
     #end
 
     #def as_json(options={})
         #super(
-        #    root: true,
-       #     methods: [:kind_description :author],
-      #      include: {kind: {only: :description}} 
-     #   )
+    #    root: true,
+    #     methods: [:kind_description :author],
+    #      include: {kind: {only: :description}} 
+    #   )
     #end
-    def hello
-        I18n.t('hello')
-    end
+    #def hello
+    #   I18n.t('hello')
+    #end
 
-    def i18n
-    I18n.default_locale        
-    end
+    #def i18n
+    #I18n.default_locale        
+    #end
 end
 
