78071b632dc5041d188acf51567d5f5b2172ca0a
Pedrog01 <pedro7109.j@gmail.com>
2023-02-10 09:53:31 -0300

    Serializer Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < ActionController::API
     
-    before_action :set_headers
- 
-    def set_headers
-      response.headers['Content-Type'] = 'application/vnd.api+json'
-    end
-    def ensure_json_request
-      if request.get? || request.delete?
-        return if request.headers['Accept'] =~ /vnd\.api\+json/
-        render nothing: true, status: 406
-      else
-        if (request.headers['Content-Type'] =~ /vnd\.api\+json/) &&
-           (request.headers['Accept'] =~ /vnd\.api\+json/)
-          return
-        end
-     
-        render nothing: true, status: 415
+  def set_headers
+    response.headers['Content-Type'] = 'application/vnd.api+json'
+  end
+  def ensure_json_request
+    if request.get? || request.delete?
+      return if request.headers['Accept'] =~ /vnd\.api\+json/
+      render nothing: true, status: 406
+    else
+      if (request.headers['Content-Type'] =~ /vnd\.api\+json/) &&
+         (request.headers['Accept'] =~ /vnd\.api\+json/)
+        return
       end
+   
+      render nothing: true, status: 415
     end
+  end
 end
