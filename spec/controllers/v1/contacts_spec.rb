cd84ec2ed6d4dbe9c2467caf9eab816f2a06341c
Pedrog01 <pedro7109.j@gmail.com>
2023-02-15 09:22:34 -0300

    Rspec (HTTP Status Code)

---
+++ spec/controllers/v1/contacts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+ describe V1::ContactsController, type: :controller do 
+    it 'request index and return 406 OK' do 
+        get :index
+        expect(response).to have_http_status(406)
+    end
+
+    it 'request index and return 200 OK' do 
+        get :index
+        expect(response).to have_http_status(200)
+    end
+end 
